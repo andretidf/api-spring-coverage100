3e18842ade9e845ec1590b3199ec5bcdcacd4e4c
Andre Muniz <andretidf@gmail.com>
2022-08-10 10:19:22 -0300

    class user resource findById

--- src/main/java/br/com/reisdigital/api/resources/UserResource.java
+++ src/main/java/br/com/reisdigital/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.reisdigital.api.resources;
 
 import br.com.reisdigital.api.domain.User;
+import br.com.reisdigital.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new User(1, "André Luiz", "andre@gmail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
