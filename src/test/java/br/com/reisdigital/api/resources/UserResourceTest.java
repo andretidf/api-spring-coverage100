48cd86727a78a0f4c46e4df79b6b023a905e4a30
Andre Muniz <andretidf@gmail.com>
2022-08-19 11:07:33 -0300

    Test findByAll implementado

--- src/test/java/br/com/reisdigital/api/resources/UserResourceTest.java
+++ src/test/java/br/com/reisdigital/api/resources/UserResourceTest.java
@@ -15,6 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +31,7 @@ class UserResourceTest {
     public static final String NAME = "André Luiz";
     public static final String EMAIL = "andre@email.com";
     public static final String PASSWORD = "234";
+    public static final int INDEX = 0;
 
     private User user;
     private UserDTO userDTO;
@@ -71,7 +74,24 @@ class UserResourceTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAListOfUserDTO() {
+        when(service.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
+
     }
 
     @Test
